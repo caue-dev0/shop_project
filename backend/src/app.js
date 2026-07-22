eb26576f63fdbf70f0aae8ab94771844d0873a19
Caue Cristian <caue.dev0@gmail.com>
2026-07-22 20:18:32 -0300

    feat: implementa middleware de tratamento de erros

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv/config";
 import express from "express";
+import bodyParser from "body-parser";
+import errorsHandling from "./middleware/errors-middleware.js";
 
 import {
   getAll,
@@ -10,29 +12,34 @@ import {
 } from "./controller/clients-controller.js";
 
 const app = express();
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  getAll(req, res);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: "100kb" }));
+
+app.get("/", (req, res, next) => {
+  getAll(req, res, next);
 });
 
-app.get("/:id", (req, res) => {
-  getById(req, res);
+app.get("/:id", (req, res, next) => {
+  getById(req, res, next);
 });
 
-app.post("/", (req, res) => {
-  postCreate(req, res);
+app.post("/", (req, res, next) => {
+  postCreate(req, res, next);
 });
 
-app.put("/:id", (req, res) => {
-  putUpdate(req, res);
+app.put("/:id", (req, res, next) => {
+  putUpdate(req, res, next);
 });
 
-app.delete("/:id", (req, res) => {
-  deleteRemove(req, res);
+app.delete("/:id", (req, res, next) => {
+  deleteRemove(req, res, next);
 });
+
+app.use(errorsHandling);
 
 app.listen(PORT, () => {
   console.log(`Server running | PORT: ${PORT}`);
--- backend/src/controller/clients-controller.js
+++ backend/src/controller/clients-controller.js
@@ -6,51 +6,51 @@ import {
   remove,
 } from "../service/clients-service.js";
 
-export async function getAll(req, res) {
+export async function getAll(req, res, next) {
   try {
     const users = await listAll();
 
     res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: "Houve algum erro interno." });
+  } catch (err) {
+    next(err);
   }
 }
 
-export async function getById(req, res) {
+export async function getById(req, res, next) {
   try {
     const user = await findById(req.params.id);
 
     res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Houve algum erro interno." });
+  } catch (err) {
+    next(err);
   }
 }
 
-export async function postCreate(req, res) {
+export async function postCreate(req, res, next) {
   try {
     const newUser = await create(req.body);
     res.status(201).json(newUser);
-  } catch (error) {
-    res.status(500).json({ message: "Houve algum erro interno." });
+  } catch (err) {
+    next(err);
   }
 }
 
-export async function putUpdate(req, res) {
+export async function putUpdate(req, res, next) {
   try {
     const updatedUser = await update(req.params.id, req.body);
 
     res.status(200).json(updatedUser);
-  } catch (error) {
-    res.status(500).json({ message: "Houve algum erro interno." });
+  } catch (err) {
+    next(err);
   }
 }
 
-export async function deleteRemove(req, res) {
+export async function deleteRemove(req, res, next) {
   try {
     await remove(req.params.id);
 
     res.status(204).json({ message: "Usuário removido com sucesso." });
-  } catch (error) {
-    res.status(500).json({ message: "Houve algum erro interno." });
+  } catch (err) {
+    next(err);
   }
 }
