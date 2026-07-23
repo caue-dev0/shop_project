4648136680f81c1e01669ec32dec226c195745d3
Caue Cristian <caue.dev0@gmail.com>
2026-07-23 19:30:35 -0300

    feat: implementa rotas personalizadas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,8 @@
 import dotenv from "dotenv/config";
-import express from "express";
+import express, { Router } from "express";
 import { errorMiddleware } from "./middleware/errors-middleware.js";
-import {
-  getAll,
-  getById,
-  postCreate,
-  putUpdate,
-  deleteRemove,
-} from "./controller/clients-controller.js";
-import { ApiError } from "./helpers/api-errors.js";
+
+import router from "./routes/router.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,25 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) => {
-  getAll(req, res, next);
-});
-
-app.get("/:id", (req, res, next) => {
-  getById(req, res, next);
-});
-
-app.post("/", (req, res, next) => {
-  postCreate(req, res, next);
-});
-
-app.put("/:id", (req, res, next) => {
-  putUpdate(req, res, next);
-});
-
-app.delete("/:id", (req, res, next) => {
-  deleteRemove(req, res, next);
-});
+app.use("/", router);
 
 app.use(errorMiddleware);
 
